6891aa4d059496bad10c56f414ab24321c324976
IvanPentyuhin <68154775+IvanPentyuhin@users.noreply.github.com>
2021-06-15 18:04:02 +0300

    add 6 homework

---
+++ lesson_six_task_2.py
@@ -0,0 +1,20 @@
+'''
+*(вместо 1) Найти IP адрес спамера и количество отправленных им запросов по данным файла логов из предыдущего задания.
+Примечание: спамер — это клиент, отправивший больше всех запросов; код должен работать даже с файлами, размер которых
+превышает объем ОЗУ компьютера.
+
+'''
+
+
+with open('nginx_logs.txt') as f:
+    date = []
+    spam_dict = {}
+    for line in f:
+        splitted = line.split()
+        date.append((splitted[0], splitted[5].replace('"', ''), splitted[6]))
+        spam_dict.setdefault(splitted[0], 0)
+        spam_dict[splitted[0]] += 1
+
+spam_dict = sorted(spam_dict.items(), key=lambda x: x[1], reverse=True)
+
+print(spam_dict[:5])
